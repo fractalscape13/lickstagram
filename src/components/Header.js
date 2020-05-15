f8e49c578e05e6f810c9886509708649c4980d95
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-15 09:28:47 -0700

    add links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
 
   return (
     <React.Fragment>
-      <h1>Lickstagram</h1><br/>
+      <Link to="/"><h1>Lickstagram</h1></Link><br/>
       <p>Share your tasty licks</p>
+      <Link to="/account">Account</Link>
+      <Link to="/">Home</Link>
     </React.Fragment>
   );
 }
