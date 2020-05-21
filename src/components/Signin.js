7d9a5b2103d5b15d51b80fd93ed0ac6a46089339
Joseph Wangemann <fractalscape13@gmail.com>
2020-05-20 21:42:43 -0700

    remove unused import statement

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../actions/index';
 import axios from 'axios';
